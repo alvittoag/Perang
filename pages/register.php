d3abf23b12dec007cabad921edbf595812aab8c0
Alvitto Aprilian Gumay <alvittogenz09@gmail.com>
2023-07-31 21:47:18 +0700

    feat : validation password

--- pages/register.php
+++ pages/register.php
@@ -21,11 +21,18 @@ if (!empty($_SESSION['username'])) {
   <?php include('../components/navbar.php') ?>
 
   <form action="register.php" method="post" class="flex justify-center flex-col items-center gap-8 mt-36">
-    <input type="text" name="username" class="border border-purple-800 py-2 w-80 rounded-md px-3 focus:outline-none" placeholder="Username" required>
+    <input type="text" name="username" class="border border-purple-800 py-2 w-80 rounded-md px-3 focus:outline-none"
+      placeholder="Username" required>
 
-    <input type="password" name="password" class="border border-purple-800 py-2 w-80 rounded-md px-3 focus:outline-none" placeholder="Password" required>
+    <input type="password" name="password" class="border border-purple-800 py-2 w-80 rounded-md px-3 focus:outline-none"
+      placeholder="Password" required>
 
-    <button type="submit" name="register" class="py-2 bg-purple-800 w-80 text-white font-bold rounded-md">Registrasi</button>
+    <input type="password" name="confirm_password"
+      class="border border-purple-800 py-2 w-80 rounded-md px-3 focus:outline-none" placeholder="Konfirmasi Password"
+      required>
+
+    <button type="submit" name="register"
+      class="py-2 bg-purple-800 w-80 text-white font-bold rounded-md">Registrasi</button>
 
   </form>
 
@@ -40,15 +47,22 @@ include('../utils/db.php');
 if (isset($_POST['register'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
 
-  $hash = password_hash($password, PASSWORD_DEFAULT);
+  if($password !== $confirm_password) {
+    echo "<script>alert('Password tidak sama')</script>";
+  } else {
 
-  $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hash')";
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+  
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hash')";
+  
+    mysqli_query($conn, $sql);
+  
+  
+    header("Location: ./login.php");
+  }
 
-  mysqli_query($conn, $sql);
-
-
-  header("Location: ./login.php");
 }
 
 ?>
